9f4275671180c1916ba817c113873211c35929b1
lucas-ohmygoal <bliu_lucas2926@outlook.com>
2022-09-07 21:50:24 +0100

    Initial commit

---
+++ simpleExample.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Lucas Liu on 2022/9/7.
+//
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void hello(){
+    cout << "hello concurrent world!" << endl;
+}
+
+int main(){
+    thread t(hello);
+    t.join();
+    cout << "hello world!" << endl;
+}
